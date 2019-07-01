0dd6a3cc3933fb72c7438a5e945e4ecba05843d4
daniel-koehler <daniel.koehler95@googlemail.com>
2019-07-01 20:52:59 +0200

    Renaming folder [1]

--- DIP I/Imaging/colorToGrayscale.m
+++
@@ -1,8 +0,0 @@
-image = imread('landscape.jpg');
-
-% different approaches for converting color to grayscale images
-average = (image(:,:,1) + image(:,:,1) + image(:,:,1))/3;
-lightness = (max(image,[],3) + min(image,[],3))/2;
-luminosity = 0.21*image(:,:,1) + 0.72*image(:,:,1) + 0.07*image(:,:,1);
-
-montage({image, average, lightness, luminosity})
--- DIP I/Imaging/imageScaling.m
+++
@@ -1,5 +0,0 @@
-image = imread('monalisa.jpg');
-
-image2 = upscaleImage(image, 1280, 856);
-image3 = scaleImage(image, 1280, 856);
-montage({image2, image3});
